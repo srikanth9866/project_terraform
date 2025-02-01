9ef7987fb4e646abed5a87028bfc64983fbfa497
srikanth karre <93827375+srikanth9866@users.noreply.github.com>
2025-02-01 15:22:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -123,3 +123,7 @@ resource "aws_instance" "my_ec2" {
     host        = self.public_ip
   }
 }
+output "public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.my_ec2.public_ip
+}
