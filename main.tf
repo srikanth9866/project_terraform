5984e2672468996cc42f7f0155d2ee9dadff492a
srikanth karre <93827375+srikanth9866@users.noreply.github.com>
2025-02-09 00:08:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -119,7 +119,7 @@ resource "aws_instance" "my_ec2" {
   connection {
     type        = "ssh"
     user        = "ec2-user"
-    private_key = file("/var/lib/jenkins/mumbai-sri.pem")  # Path on the Jenkins server
+    private_key = file("/var/lib/jenkins/srikanth0370.pem")  # Path on the Jenkins server
     host        = self.public_ip
   }
 }
